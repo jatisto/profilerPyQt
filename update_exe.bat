9f14bfae0ee675565b0ed1b125581cb0f97e5edb
eabdyushev <eabdyushev@nobilis.team>
2023-08-29 10:56:42 +0600

    27-MAJOR.MINOR.PATCH AND GLOBAL REANIMATION UPDATE V28

--- update_exe.bat
+++ update_exe.bat
@@ -3,5 +3,5 @@
 rem Путь к файлу update_exe.ps1
 set "ps1_file_path=%~dp0update_exe.ps1"
 
-rem Запуск PowerShell скрипта с правами администратора
-powershell -ExecutionPolicy Bypass -File "%ps1_file_path%"
+rem Запуск PowerShell скрипта в отдельном процессе
+start powershell -NoProfile -ExecutionPolicy Bypass -File "%ps1_file_path%"

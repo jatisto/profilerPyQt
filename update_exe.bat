0482511f39709676946b725eacd2c43410e00295
eabdyushev <eabdyushev@nobilis.team>
2023-08-29 10:43:52 +0600

    27-MAJOR.MINOR.PATCH AND GLOBAL REANIMATION UPDATE V27

--- update_exe.bat
+++ update_exe.bat
@@ -1,31 +1,7 @@
 @echo off
-setlocal
 
-set "LOG_FILE=%ProgramFiles%\PgStatStatementsReaderQt5\bat_update_log.txt"
+rem Путь к файлу update_exe.ps1
+set "ps1_file_path=%~dp0update_exe.ps1"
 
-echo Copying files from PgStatStatementsReaderQt5 to temporary folder >> %LOG_FILE%
-mkdir %TMP%\tmp_update_folder
-xcopy /s /y "%ProgramFiles%\PgStatStatementsReaderQt5\*" "%TMP%\tmp_install\"
-
-echo Closing PgStatStatementsReaderQt5.exe processes >> %LOG_FILE%
-taskkill /f /im PgStatStatementsReaderQt5.exe
-
-echo Replacing files >> %LOG_FILE%
-xcopy /s /y "%TMP%\tmp_update_folder\profilerPyQt\profilerPyQt-main\build\PgStatStatementsReaderQt5\*" "%ProgramFiles%\PgStatStatementsReaderQt5\"
-
-echo Checking for successful file replacement >> %LOG_FILE%
-if %error level%==0 (
-    echo Deleting temporary files >> %LOG_FILE%
-    rmdir /s /q %TMP%\tmp_update_folder
-    rmdir /s /q %TMP%\tmp_install
-) else (
-    echo Restoring files from temporary folder >> %LOG_FILE%
-    xcopy /s /y "%TMP%\tmp_update_folder\*" "%ProgramFiles%\PgStatStatementsReaderQt5\"
-    echo Deleting temporary files >> %LOG_FILE%
-    rmdir /s /q %TMP%\tmp_update_folder
-)
-
-echo Running PgStatStatementsReaderQt5.exe >> %LOG_FILE%
-start "" "%ProgramFiles%\PgStatStatementsReaderQt5\PgStatStatementsReaderQt5.exe"
-
-endlocal
+rem Запуск PowerShell скрипта с правами администратора
+powershell -ExecutionPolicy Bypass -File "%ps1_file_path%"
